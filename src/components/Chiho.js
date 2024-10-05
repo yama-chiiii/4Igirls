8a42c61afc65b0d178a11ff0cccc95d334b547db
yama-chiiii <nagayama0409@gmail.com>
2024-10-06 00:18:35 +0900

    みゃ

--- src/components/Chiho.js
+++ src/components/Chiho.js
@@ -1,9 +1,15 @@
 export const Chiho = () => {
     return (
         // ながやま専用ページだよ
-        //色々適当にやってるから参考にしてみてねん
-        <div>
-            <h1>やっほう</h1>
+        <div className="w-full h-screen flex flex-col justify-center items-center bg-pink-100">
+            <div className="w-3/4 h-screen bg-white flex flex-col justify-center items-center">
+                <div className="text-4xl my-8 text-pink-500 font-black">くるくるはーと</div>
+                <div className="scene relative">
+                    <div className="regular-tetrahedron transform-style-3d animate-rotate sticky top-[25dvh] w-120 h-120 flex justify-center items-center">
+                        <div className="text-7xl text-red-600">♥</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        rotate: {
+          '0%': { transform: 'rotateX(0deg) rotateY(0deg)' },
+          '100%': { transform: 'rotateX(360deg) rotateY(360deg)' },
+        },
+      },
+      animation: {
+        rotate: 'rotate 10s infinite linear',
+      },
+    },
   },
   plugins: [],
 }
